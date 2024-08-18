e3ca1b8fc2a6cc8c20dc1bfa7aba85a3cf7cd8d8
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-08-18 14:47:27 +0600

    navbar some customization

--- src/Firebase/firebase.confige.js
+++ src/Firebase/firebase.confige.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-0iVAUa_ui2iUOWAKH4Sr2DF6CLA6D7I",
-  authDomain: "product-peak-fe9b7.firebaseapp.com",
-  projectId: "product-peak-fe9b7",
-  storageBucket: "product-peak-fe9b7.appspot.com",
-  messagingSenderId: "559990392600",
-  appId: "1:559990392600:web:064e132019b4b22768806d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MASSEGINFSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
